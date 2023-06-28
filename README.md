abe951cb0a74ffd2e27d21b4385756cc2c1e09e7
parkminwoo <parkminwoo1991@gmail.com>
2023-06-28 18:22:44 +0900

    init: commit and release 0.1.0

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
+Development Status :: 3 - Alpha
+
 # arxiv2text
 converting PDF files to Word documents, mainly with a focus on arXiv papers.
---
+++ arxiv2text/__init__.py
@@ -0,0 +1,8 @@
+# Copyright 2023 parkminwoo, MIT License
+
+import os
+
+__all__ = [
+]
+__version__ = "0.1.0"
+__author__ = "daniel park <parkminwoo1991@gmail.com>"
