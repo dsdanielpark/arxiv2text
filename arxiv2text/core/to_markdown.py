31c9a5b2eab60b465695119b4ba39367957be27d
parkminwoo <parkminwoo1991@gmail.com>
2023-10-23 23:25:51 +0900

    chore: style black

--- arxiv2text/core/to_markdown.py
+++ arxiv2text/core/to_markdown.py
@@ -4,6 +4,7 @@ import requests
 from pdfminer.high_level import extract_text
 from pdfminer.layout import LAParams
 
+
 def arxiv_to_md(pdf_url: str, output_folder: str) -> None:
     """
     Extracts the Abstract and Introduction sections from a PDF from an arXiv URL
@@ -27,7 +28,9 @@ def arxiv_to_md(pdf_url: str, output_folder: str) -> None:
     filename = os.path.join(output_folder, pdf_url.split("/")[-1] + ".md")
 
     # Extract text from the PDF
-    extracted_text = extract_text(io.BytesIO(requests.get(pdf_url).content), laparams=LAParams())
+    extracted_text = extract_text(
+        io.BytesIO(requests.get(pdf_url).content), laparams=LAParams()
+    )
 
     # Find the start and end of the Abstract and Introduction sections
     start_abstract = extracted_text.find("Abstract")
@@ -36,7 +39,9 @@ def arxiv_to_md(pdf_url: str, output_folder: str) -> None:
 
     # Extract Abstract and Introduction sections
     abstract = extracted_text[start_abstract:end_abstract].strip()
-    introduction = extracted_text[start_introduction:].strip() if start_introduction != -1 else ""
+    introduction = (
+        extracted_text[start_introduction:].strip() if start_introduction != -1 else ""
+    )
 
     # Save the Abstract and Introduction as Markdown
     markdown_text = f"# {abstract}\n\n# {introduction}"
