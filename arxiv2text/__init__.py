684617b3634d60673aef979ebe957077cb0785ca
parkminwoo <parkminwoo1991@gmail.com>
2023-10-27 17:34:30 +0900

    chore: package info

--- arxiv2text/__init__.py
+++ arxiv2text/__init__.py
@@ -4,7 +4,8 @@ from arxiv2text.convert.to_html import arxiv_to_html
 from arxiv2text.convert.to_markdown import arxiv_to_md
 from arxiv2text.search import fetch_arxiv_papers
 
+__author__ = "daniel park <parkminwoo1991@gmail.com>"
+__version__ = "0.1.5"
 __all__ = ["arxiv_to_text", "arxiv_to_html", "arxiv_to_md", "fetch_arxiv_papers"]
 
-__version__ = "0.1.5"
-__author__ = "daniel park <parkminwoo1991@gmail.com>"
+
