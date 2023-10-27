3c7beeff82011b8c051c84f426a858733f548903
parkminwoo <parkminwoo1991@gmail.com>
2023-10-28 08:45:14 +0900

    build: 0.1.7

--- arxiv2text/__init__.py
+++ arxiv2text/__init__.py
@@ -3,7 +3,9 @@ from arxiv2text.convert.to_text import arxiv_to_text
 from arxiv2text.convert.to_html import arxiv_to_html
 from arxiv2text.convert.to_markdown import arxiv_to_md
 from arxiv2text.search import fetch_arxiv_papers
+from arxiv2text.utils import find_most_similar_subject
+from arxiv2text.constant import SUBJECTS
 
 __author__ = "daniel park <parkminwoo1991@gmail.com>"
-__version__ = "0.1.6"
-__all__ = ["arxiv_to_text", "arxiv_to_html", "arxiv_to_md", "fetch_arxiv_papers"]
+__version__ = "0.1.7"
+__all__ = ["arxiv_to_text", "arxiv_to_html", "arxiv_to_md", "fetch_arxiv_papers", "find_most_similar_subject", "SUBJECTS"]
