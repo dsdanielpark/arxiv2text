17dfba06b3c9d81ab766b5c0b1713909d442d82d
parkminwoo <parkminwoo1991@gmail.com>
2023-10-27 16:26:15 +0900

    build: 0.1.4

--- arxiv2text/__init__.py
+++ arxiv2text/__init__.py
@@ -1,9 +1,10 @@
 # Copyright 2023 parkminwoo, MIT License
-from arxiv2text.core.to_text import arxiv_to_text
-from arxiv2text.core.to_html import arxiv_to_html
-from arxiv2text.core.to_markdown import arxiv_to_md
+from arxiv2text.convert.to_text import arxiv_to_text
+from arxiv2text.convert.to_html import arxiv_to_html
+from arxiv2text.convert.to_markdown import arxiv_to_md
+from arxiv2text.search import fetch_arxiv_papers
 
-__all__ = ["arxiv_to_text", "arxiv_to_html", "arxiv_to_md"]
+__all__ = ["arxiv_to_text", "arxiv_to_html", "arxiv_to_md", "fetch_arxiv_papers"]
 
-__version__ = "0.1.3"
+__version__ = "0.1.4"
 __author__ = "daniel park <parkminwoo1991@gmail.com>"
