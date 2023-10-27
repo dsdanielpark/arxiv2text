f0bbbe4f662e7a54e4ff5e6382e2855973bc89d1
parkminwoo <parkminwoo1991@gmail.com>
2023-10-27 17:40:48 +0900

    doc: updated

--- arxiv2text/search.py
+++ arxiv2text/search.py
@@ -5,42 +5,53 @@ from arxiv2text.utils import replace_enter_to_space
 
 def fetch_arxiv_papers(subject: str, max_results: int, print_bool: bool = False) -> dict:
     """
-    Fetch paper information from arXiv using the provided subject and maximum number of results.
+    Fetch a list of arXiv papers based on a subject and optional criteria.
 
     Args:
-        subject (str): The subject to search on arXiv.
-        max_results (int): The maximum number of paper results to retrieve.
-        print_bool (bool, optional): Whether to print the results. Default is False.
+    subject (str): The subject or topic of the papers to search for.
+    max_results (int): The maximum number of papers to retrieve (default is 2).
+    print_bool (bool): Whether to print the retrieved paper information (default is False).
 
     Returns:
-        dict: A dictionary containing the fetched paper information.
-
-    Example:
-        >>> subject = "Computer Science"
-        >>> papers = fetch_arxiv_papers(subject, max_results=2, print_bool=True)
+    dict: A dictionary containing information about the retrieved papers.
+        The dictionary has the following structure for each paper:
+        {
+            'Title': str,        # Title of the paper
+            'Abstract': str,     # Abstract or summary of the paper
+            'URL': str,          # URL link to the paper
+            'Published': str,    # Date the paper was published
+            'DOI': str           # Digital Object Identifier of the paper
+        }
     """
     search_query = f'all:{subject.replace(" ", "+")}'
     url = f'http://export.arxiv.org/api/query?search_query={search_query}&start=0&max_results={max_results}'
     response = urllib.request.urlopen(url)
     data = response.read().decode('utf-8')
-    namespace = {'atom': 'http://www.w3.org/2005/Atom', 'opensearch': 'http://a9.com/-/spec/opensearch/1.1'}
+    namespace = {'atom': 'http://www.w3.org/2005/Atom', 'opensearch': 'http://a9.com/-/spec/opensearch/1.1', 'arxiv': 'http://arxiv.org/schemas/atom'}
     root = ET.fromstring(data)
     entry_dict = {}
+
     for i, entry in enumerate(root.findall('.//atom:entry', namespaces=namespace)[:max_results]):
         entry_info = {}
         entry_info['Title'] = replace_enter_to_space(entry.find('./atom:title', namespaces=namespace).text)
-        entry_info['Link'] = replace_enter_to_space(entry.find('./atom:id', namespaces=namespace).text)
-        summary = entry.find('./atom:summary', namespaces=namespace).text.strip()
-        summary = replace_enter_to_space(summary)
-        entry_info['Summary'] = summary
+        abstract = entry.find('./atom:summary', namespaces=namespace).text.strip()
+        abstract = replace_enter_to_space(abstract)
+        entry_info['Abstract'] = abstract
+        entry_info['URL'] = replace_enter_to_space(entry.find('./atom:id', namespaces=namespace).text)
+        published = entry.find('./atom:published', namespaces=namespace).text
+        entry_info['Published'] = published
+        doi = entry.find('./arxiv:doi', namespaces=namespace).text
+        entry_info['DOI'] = doi
         entry_dict[i] = entry_info
 
     if print_bool:
         for i, entry in entry_dict.items():
-            print(f"Paper {i + 1}:")
+            print(f"Paper {i + 1}")
             print("Title:", entry['Title'])
-            print("Link:", entry['Link'])
-            print("Summary:", entry['Summary'])
+            print("Abstract:", entry['Abstract'])
+            print("URL:", entry['URL'])
+            print("Published:", entry['Published'])
+            print("DOI:", entry['DOI'])
             print("\n")
 
     return entry_dict
