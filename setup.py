abd84b54e698aea5ea3271a81731f06c1133239d
MinWoo(Daniel) Park <parkminwoo1991@gmail.com>
2023-11-04 15:26:24 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
-        "License :: OSI Approved :: MIT License",
+        "License :: OSI Approved :: Apache License",
         "Programming Language :: Python :: 3.6",
         "Topic :: Scientific/Engineering :: Artificial Intelligence",
     ],
